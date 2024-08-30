91ee6feb896f06647916d3acd249690042a2521f
diego.becco <diego.becco@dcsg.com>
2024-08-30 11:35:49 -0300

    First condition tested

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,9 +1,17 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-    it('should foo', () => {
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    it('should decrease quality and sellIn', () => {
+        const gildedRose = new GildedRose([new Item('foo', 1, 1)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe('fixme');
+        expect(items[0].sellIn).toBe(0);
+        expect(items[0].quality).toBe(0);
+    });
+
+    it('given sellIn is zero, quality degrades as fast', () => {
+        const gildedRose = new GildedRose([new Item('random item', 0, 20)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(-1);
+        expect(items[0].quality).toBe(18);
     });
 });
