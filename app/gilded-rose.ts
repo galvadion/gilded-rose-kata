905a279f06dc79ad51fcd4f528b2a6b4234c13f9
diego.becco <diego.becco@dcsg.com>
2024-08-30 12:39:39 -0300

    Update quality handles >0

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,7 +10,8 @@ export class Item {
     }
 
     updateQuality(){
-        this.quality--;
+        if(this.quality>0)
+            this.quality--;
     }
     updateSellIn(){
         this.sellIn--;
@@ -55,10 +56,8 @@ export class GildedRose {
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].updateQuality()
-                            }
+                        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                            this.items[i].updateQuality()
                         }
                     } else {
                         this.items[i].quality = this.items[i].quality - this.items[i].quality
